b403fb4be17ac44f1da80e26d4cd7019f4663c61
Maruf-Raju-pro <Muhammedmarufraju@gmail.com>
2024-03-20 12:48:28 +0600

    first commit

---
+++ src/routes/contact.jsx
@@ -0,0 +1,78 @@
+import { Form } from 'react-router-dom';
+
+export default function Contact() {
+  const contact = {
+    first: 'Muhammed',
+    last: 'Maruf',
+    avatar:
+      'https://i.postimg.cc/QdhpKmDd/412757623-122110182440151422-5252159568520494002-n.jpg',
+    twitter: 'https://www.facebook.com/MarufBytes/',
+    notes:
+      'Enthusiastic junior React developer eager to innovate and grow in web development',
+    favorite: true,
+  };
+
+  return (
+    <div id="contact">
+      <div>
+        <img key={contact.avatar} src={contact.avatar || null} />
+      </div>
+
+      <div>
+        <h1>
+          {contact.first || contact.last ? (
+            <>
+              {contact.first} {contact.last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}{' '}
+          <Favorite contact={contact} />
+        </h1>
+
+        {contact.twitter && (
+          <p>
+            <a target="_blank" href={`https://twitter.com/${contact.twitter}`}>
+              {contact.twitter}
+            </a>
+          </p>
+        )}
+
+        {contact.notes && <p>{contact.notes}</p>}
+
+        <div>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
+          <Form
+            method="post"
+            action="destroy"
+            onSubmit={event => {
+              if (!confirm('Please confirm you want to delete this record.')) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Favorite({ contact }) {
+  // yes, this is a `let` for later
+  let favorite = contact.favorite;
+  return (
+    <Form method="post">
+      <button
+        name="favorite"
+        value={favorite ? 'false' : 'true'}
+        aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}
+      >
+        {favorite ? '★' : '☆'}
+      </button>
+    </Form>
+  );
+}
